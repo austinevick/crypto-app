af7957ec64899844da2be061fd57ab59a3a1382e
Augustine Victor <51150248+austinevick@users.noreply.github.com>
2020-09-03 19:25:42 +0100

    change codes

---
+++ lib/models/model.dart
@@ -0,0 +1,8 @@
+class Movie {
+  final String title, posterPath, overview;
+  Movie({this.title, this.posterPath, this.overview});
+  Movie.fromJson(Map json)
+      : title = json['title'],
+        posterPath = json['poster_path'],
+        overview = json['overview'];
+}
